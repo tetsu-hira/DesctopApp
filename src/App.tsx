ed504c7f1473c579d93c7ae7e9dbf363f55200c5
tetsu.hira <svc.snowdrop@gmail.com>
2022-04-18 16:56:17 +0900

    Initial Comit

---
+++ src/App.tsx
@@ -0,0 +1,121 @@
+import './App.css';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+
+import allActions from './actions';
+import Header from './components/Header';
+import Index from './components/Index';
+import Item from './components/Item';
+import Process from './components/Process';
+import Product from './components/Product';
+import Test from './components/Test';
+
+type Doc = {
+  id: number;
+  name: string;
+};
+
+const App: React.FC = () => {
+  const [docu, setDocu] = useState<string>('');
+  const [id, setId] = useState<number>(0);
+  const [list, setList] = useState<Doc[]>([]);
+  const [note, setNote] = useState<string | null>('');
+  const [item, setItem] = useState<string>('');
+
+  const entryItem = useSelector((state: any) => state.entryItem);
+  const dispatch = useDispatch();
+  const itemName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (item) {
+      if (item.length < 33) {
+        setItem(event.target.value);
+        console.log(item);
+      }
+    }
+  };
+  const defaultItem = () => {
+    setItem(item.slice(1));
+    console.log(item);
+  };
+  const nullItem = () => {
+    setItem(' ');
+    console.log(item);
+  };
+
+  const nullDocu = () => {
+    setDocu(' ');
+    console.log(docu);
+  };
+  const defaultDocu = () => {
+    setDocu(docu.slice(1));
+  };
+
+  console.log(entryItem.itemList);
+
+  // ドキュメントの名前を入力して登録する
+  const docuName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (docu.length < 33) {
+      setDocu(event.target.value);
+      console.log(docu);
+    }
+  };
+  // console.log(list);
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <div className='App'>
+        <div className='Top'>
+          <div className='TopContainer'>
+            <div className='TopContainer__Left'>
+              {/* Reduxを使ったテスト */}
+              <div className='TopHead'>
+                <div className='TopButton'>
+                  <input
+                    className='TopButton__name'
+                    type='text'
+                    id='name'
+                    onChange={itemName}
+                    value={!item ? '作成したい大会・部門名を入力' : item}
+                    onBlur={defaultItem}
+                    onClick={nullItem}
+                  ></input>
+                  <button
+                    onClick={() => dispatch(allActions.entryAction.addItem(item))}
+                    className='TopButton__button'
+                  >
+                    Create
+                  </button>
+                </div>
+                <div className='TopHead__title'>＜Category-List＞</div>
+              </div>
+              <ul className='TopList'>
+                {entryItem.itemList.map((item: string) => (
+                  <li key={item} className='TopList__item'>
+                    <div className='TopList__itemName'>
+                      <Link to={`./New2set/${item}`} className='TopList__itemName'>
+                        <p>{item}</p>
+                      </Link>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className='TopContainer__Right'>
+              <Routes>
+                <Route path='/New2set' element={<Index />} />
+                <Route path='/top' element={<Process />} />
+                <Route path='/test' element={<Test />} />
+                <Route path='/:id/:name' element={<Product />} />
+                <Route path='/New2set/:item' element={<Item />} />
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <Footer /> */}
+    </BrowserRouter>
+  );
+};
+
+export default App;
